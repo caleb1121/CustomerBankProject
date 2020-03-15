5643acf4671b63f2b4a98a1228ed99268df47a76
CalebPhillips <calebphillips132@gmail.com>
2020-03-15 20:58:57 +0000

    Added Customer class. First pass on Customer generation system (only generating unique ID) Customers now saved to a list Added ability to loop through and generate as many customers as you like Added ability to print all customers to console

---
+++ CustomerBankProject/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerBankProject
+{
+    class Customer
+    {
+        
+
+
+        public int UniqueID { set; get; } // Used to find people 
+
+        public string FirstName { set; get; }
+
+        public string LastName { set; get; }
+
+        public int CustomerMoney { set; get; } // The amount of money the person walks into the bank with
+
+        public int BaseAcceptanceChance { set; get; } // The base acceptance chance of the person accepting a bank account
+
+
+        public Customer(int ID)
+
+        {
+            UniqueID = ID;
+
+            
+        }
+    }
+}
--- CustomerBankProject/Program.cs
+++ CustomerBankProject/Program.cs
@@ -11,6 +11,36 @@ namespace CustomerBankProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            int PersonIDCounter = 0;
+            List<Customer> Customers = new List<Customer>(); // used to store all the customers generated
+
+
+
+
+            for (int i = 0; i < 101; i++) //generate 100 customers
+            {
+
+                //generate new customer and add to list
+                Customers.Add(new Customer(PersonIDCounter));
+                // finish generating and adding to list
+
+                
+                Console.WriteLine("Generated user:" + PersonIDCounter); //print last customer generated
+                PersonIDCounter++; // don't be dumb
+
+
+            }
+
+
+            
+
+
+
+            foreach (Customer customer in Customers)
+            {
+                Console.WriteLine(customer.UniqueID);
+            }
+
         }
     }
 }
