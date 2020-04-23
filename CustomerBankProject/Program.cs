f6099de87a44b05265176b42d2ce23527ee333b2
Caleb Phillips <calebphillips132@gmail.com>
2020-04-23 18:12:17 +0100

    Removed all the writeline commands throughout the bank simulation day Added a progress counter so the user can see the current bank day progress Started to add an end of day report for the bank simulation

--- CustomerBankProject/Program.cs
+++ CustomerBankProject/Program.cs
@@ -19,47 +19,21 @@ namespace CustomerBankProject
 
 
             Console.WriteLine("Welcome to the banking app");
-            Console.WriteLine("How many customers would you like to generate?");
+            Console.WriteLine("How many customers would you like to generate? (1-500 for best performance)");
            
             string generateCustomerNum = Console.ReadLine(); // Set the amount of customers to generate
-
-
-
-            Console.WriteLine("type 1 for manual mode"); // used to allow the user to generate users by button press
-
-            string manORautochoice = Console.ReadLine();
-
-
-
-            Console.WriteLine("type 1 for easy read mode"); // used to allow the user to generate users by button press
-
-            string easyReadchoice = Console.ReadLine();
-
-
-
-            Console.Clear();
-            
-
-            
+            Console.Clear();           
             
             int generateCustomerInt; // create variable to store conversion from string to int
             generateCustomerInt = Convert.ToInt32(generateCustomerNum); // convert to int
-
-
 
             int PersonIDCounter = 0; // incremental counter "customer1 customer2... etc"
             List<Customer> Customers = new List<Customer>(); // used to store all the customers generated using a list
 
 
-
-
             for (int i = 0; i < generateCustomerInt; i++) //generate specified number of customers
             {
-
-                Console.Clear();
-                
-                
-                
+               
                 // if statements below used to set console colour to represent progress of generating customers
                 
                 if(i < generateCustomerInt * 0.25)
@@ -72,9 +46,7 @@ namespace CustomerBankProject
                 { Console.ForegroundColor = ConsoleColor.Yellow; }
 
                 else if (i < generateCustomerInt * 0.99)
-                  { Console.ForegroundColor = ConsoleColor.Green; }
-
-
+                { Console.ForegroundColor = ConsoleColor.Green; }
 
 
                 //generate new customer and add to list
@@ -103,6 +75,8 @@ namespace CustomerBankProject
             {
                 while(runProgram)
                 {
+                    Console.Clear(); // used to remove sim progress text 
+
                     Console.WriteLine("Please choose one of the following options:");
                     Console.WriteLine("1. Simulate next day");
                     Console.WriteLine("2. search bank records");
@@ -125,12 +99,10 @@ namespace CustomerBankProject
 
                     void simulateBankDay()
                     {
-
+                        int simProgress = 0; // used to store current sim progress
 
                         foreach (Customer customer in Customers) //used to loop through every customer and perform action
                         {
-
-
 
                             Random rndDepChoiceNum = new Random();
                             int depChoice;
@@ -139,94 +111,62 @@ namespace CustomerBankProject
 
                             depChoice = rndDepChoiceNum.Next(1, 100); // compared to "baseacceptancechance" to decide whether the customer stays with the bank
 
-                            Thread.Sleep(250);
+                            Thread.Sleep(10); // used to slow sim down to help random number generator
 
 
 
                             if (customer.BaseAcceptanceChance > depChoice) // The customer wishes to take out an account
-                            {
-
-                                Console.WriteLine(customer.FirstName + " walked into the bank");
+                            {                               
 
                                 Thread.Sleep(1); // can be used to slow program down to make it easier to read
 
 
 
                                 int depositPercentageInt; // how much of the customers money they wish to deposit
-                                float depositPercentageFloat; // same as above butused to convert it into a float
+                                float depositPercentageFloat; // same as above but used to convert it into a float
                                 depositPercentageInt = rndDepChoiceNum.Next(10, 70); // after division will be between 0.1 to 0.7
-                                depositPercentageFloat = depositPercentageInt / 100.0f;
+                                depositPercentageFloat = depositPercentageInt / 100.0f; // this is the division
 
                                 int depositAmount = Convert.ToInt32(customer.CustomerMoney * depositPercentageFloat); // calculate deposit amount
 
 
-                                Console.WriteLine("The customer has: £" + customer.CustomerMoney + " and would like to deposit: £" + depositAmount); // print the total money and how much the customer wants to deposit
+                                
 
-
-                                Console.WriteLine("Convincing customer to deposit more money...");
-
-                                int depositNegotiate;
+                                int depositNegotiate; // used to decide whether the customer is convinced to deposit more money by the bank teller
                                 depositNegotiate = rndDepChoiceNum.Next(0, 2); // random number used to decide whether the user deposits more money
 
                                 if (depositNegotiate == 1) // deposit more
                                 {
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.WriteLine("Success!");
-                                    Console.ResetColor();
-
-                                    depositAmount = depositAmount + depositAmount / 10; // calculate new deposit amount
-
-                                    Console.WriteLine("New deposit amount: £" + depositAmount);
+                                    depositAmount = depositAmount + depositAmount / 10; // calculate new deposit amount                                    
                                 }
 
                                 else // don't deposit more
                                 {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("Failed...");
-                                    Console.ResetColor();
+
                                 }
 
+                               
                                 customer.CustomerBankAccountMoney = customer.CustomerBankAccountMoney + depositAmount; // customer money is put into the bank
                                 customer.CustomerMoney = customer.CustomerMoney - depositAmount; // removing customer money from customer (because it is now in the bank account)
 
-
-                                Console.WriteLine("Customer has deposited money");
-                                Console.WriteLine("New account balance: £" + customer.CustomerBankAccountMoney);
-                                Console.WriteLine("Money on customer: £" + customer.CustomerMoney);
-
-
-
                             }
 
 
 
-                            else // customer leaves the bank without opening an account
+                            else // customer walks past the bank instead
                             {
-                                Console.WriteLine(customer.FirstName + " " + customer.LastName + " walked past the bank...");
-                                Console.ResetColor();
-                            }
-
-
-
-
-                            Console.WriteLine("");
-
-                            if (manORautochoice == "1") // used to pause after each customer
-                            {
-                                Console.WriteLine("");
-                                Console.WriteLine("Press any key to generate next customer...");
-                                Console.ReadKey();
-
-                                if (easyReadchoice == "1")
-                                { Console.Clear(); }
-
-
-                                Console.ReadKey(); 
 
                             }
 
-                             
+                            
+                            simProgress = simProgress + 1; // plus one to current progress
+                            Console.Clear(); // clear console before displaying new progress
+                            Console.WriteLine("Sim progress: " + simProgress + "/" + generateCustomerNum); // display current progress
+
                         } //simulate a bank day
+
+
+                        Console.WriteLine("End of day report:");
 
                     }
 
